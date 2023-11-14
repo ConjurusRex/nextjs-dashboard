aca25bcba1a1ae336169af9b7a96a13616607036
Josh <joshua.nebeker@mx.com>
2023-11-13 17:31:48 -0700

    feat: fin search and pagination, adds action to create invoice, update invoice checkpoint.

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -16,15 +16,15 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
-  // NOTE: comment in this code when you get to this point in the course
+  // NOTE: comment in this code when you get to this point in the course - noted
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
+      {/* NOTE: comment in this code when you get to this point in the course - noted */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -57,7 +57,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
